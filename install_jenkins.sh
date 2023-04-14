4cc2de0fb51d7ebffeef3a8b7cdfed24e01bca52
asecurityguru <100665517+asecurityguru@users.noreply.github.com>
2023-04-14 12:30:10 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,7 +4,7 @@
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 #sudo yum install jenkins java-1.8.0-openjdk-devel -y
 sudo amazon-linux-extras install java-openjdk11
