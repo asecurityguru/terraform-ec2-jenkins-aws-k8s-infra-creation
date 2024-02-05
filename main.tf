12500ebc9ca8e62cbb68125280e04d68d2389b48
asecurityguru <100665517+asecurityguru@users.noreply.github.com>
2024-02-05 19:24:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0*"]
+    values = ["al2023-ami-2023.3.20240131.0-kernel-6.1-x86_64*"]
   }
 
   filter {
