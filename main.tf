973291dc6c79fe8f8151e5cb4147aa95adb439a8
asecurityguru <100665517+asecurityguru@users.noreply.github.com>
2024-07-05 12:04:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["*al2023-ami-2023.3.*-kernel-6.1-x86_64*"]
+    values = ["*al2023-ami-2023.5.*-kernel-6.1-x86_64*"]
   }
 
   filter {
